7cdb350da3758cd0c3af2bdc4ab3fba0cd06679a
AmIWatchU <user106512@gmail.com>
2022-07-10 18:51:35 +0300

    Add project files.

---
+++ WebApplication/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using WebApplication.Models;
+using WebApplication.Services;
+
+namespace WebApplication.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+      
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+
+
+    }
+}
